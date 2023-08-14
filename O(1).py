92692b39b70a6c5c2f2b3944b55669b5f92270dc
saeed jeddi <saeedjeddimail@gmail.com>
2023-08-14 15:54:47 +0330

    algorithm o1

---
+++ O(1).py
@@ -0,0 +1,18 @@
+"""
+constant Time  O(1)
+
+
+
+
+            
+"""
+    
+nums = [2, 10, 4, 49, 6, 127, 8, 1002, 10]
+
+def show(list1):
+    if list1[0] % 2 == 0:
+        return'Event'
+    return 'odd'
+
+print(show(nums))
+
